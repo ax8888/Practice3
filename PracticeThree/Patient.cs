b5f3dca7b4b9d5c7ee96266a706905b596725d26
ax8888 <adriana0villarroel@gmail.com>
2023-05-04 22:06:26 -0400

    P3-01: Created https post,put,delete,get, getbyci

---
+++ PracticeThree/Patient.cs
@@ -0,0 +1,11 @@
+namespace PracticeThree;
+
+
+public class Patient{
+    public string Name {set; get;}
+    public string LastName {get; set;}
+    public long CI {get; set;}
+    public string bloodInformation {get;set;}
+
+}
+
