81492cb6f1fac08566a0bb799019ea76ac00cd1f
ax8888 <adriana0villarroel@gmail.com>
2023-05-04 21:21:27 -0400

    P3-01 Added changes to Program.cs to let other environment works as well as have a changing title depending on the environment in usage.

--- PracticeThree/Program.cs
+++ PracticeThree/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,10 +10,38 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Title
+var configurationBuilder = new ConfigurationBuilder()
+.SetBasePath(builder.Environment.ContentRootPath)
+.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+.AddEnvironmentVariables(); 
+
+IConfiguration Configuration = configurationBuilder.Build();
+
+string siteTitle = Configuration.GetSection("Title").Value;
+
+builder.Services.AddSwaggerGen(options => 
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = siteTitle
+    });
+});
+
+//Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+builder.Host.UseSerilog();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "QA" ||
+app.Environment.EnvironmentName == "UAT")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
